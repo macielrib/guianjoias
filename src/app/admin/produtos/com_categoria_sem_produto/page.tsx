636fd9e2cba86962f4ace60d92d195b9f4025243
Maciel <112631220+macielrib@users.noreply.github.com>
2024-10-01 07:52:52 -0300

    test

--- src/app/admin/produtos/com_categoria_sem_produto/page.tsx
+++ src/app/admin/produtos/com_categoria_sem_produto/page.tsx
@@ -3,12 +3,10 @@ import Link from "next/link";
 import { BsPatchCheckFill } from "react-icons/bs";
 import { FiTrash2 } from "react-icons/fi";
 
-// Definindo a interface para a categoria
 interface Category {
   name: string;
 }
 
-// Definindo a interface de props que o componente espera
 interface Props {
   categories: Category[];
 }
@@ -18,7 +16,6 @@ const CategoriesWithoutProducts = ({ categories }: Props) => {
     <>
       <div className="flex flex-col -mt-8 px-4 lg:px-56 w-full">
         <div className="bg-[#F2DD52] rounded-md w-full max-w-6xl md:w-[74rem] h-4 md:ml-6 mt-4"></div>
-
         {/* Badge: Você está gerenciando... */}
         <div className="w-full max-w-5xl bg-transparent py-2 px-6 rounded-lg mt-4 flex items-center gap-3 justify-start">
           <BsPatchCheckFill className="text-[#F2DD52] text-4xl sm:text-xl" />
@@ -39,7 +36,6 @@ const CategoriesWithoutProducts = ({ categories }: Props) => {
           </p>
         </div>
       </div>
-
       <div className="flex flex-col md:flex-row md:ml-32 gap-8 px-6 items-center md:items-start justify-center">
         <div className="flex flex-col w-full max-w-[340px] h-full max-h-[545px] rounded-lg bg-[#171717]">
           <div className="flex flex-col py-6 px-6 items-start justify-start">
@@ -51,7 +47,7 @@ const CategoriesWithoutProducts = ({ categories }: Props) => {
                 type="text"
                 placeholder="Nome da categoria"
                 className="border !border-[#4F4F4F] bg-transparent rounded-full py-4 px-3 pl-10 text-[#4F4F4F] placeholder:text-[#4F4F4F] font-poppins font-normal text-sm w-[270px] h-[48px] focus:border-[#F2DD52] focus:outline-none transition duration-300"
-                maxLength={9}
+                maxLength={9} 
               />
             </div>
             <div className="flex items-center mb-4 py-4 px-2 text-white">
@@ -88,10 +84,12 @@ const CategoriesWithoutProducts = ({ categories }: Props) => {
                       href={`/category-id`}
                       className="flex justify-between w-full max-w-sm rounded-lg py-3 px-8 items-center mb-4 border-2 border-[#F2DD52]"
                     >
-                      <h1 className="font-poppins text-white font-medium text-md">
+                      <h1 className="font-poppins text-white font-medium text-md ">
                         {category.name}
                       </h1>
-                      <button className="text-white transition duration-300 hover:-translate-y-1">
+                      <button
+                        className=" text-white transition duration-300 hover:-translate-y-1"
+                      >
                         <FiTrash2 className="h-5 w-5" />
                       </button>
                     </Link>
@@ -109,7 +107,7 @@ const CategoriesWithoutProducts = ({ categories }: Props) => {
 
         {/* Seção de Produtos na Categoria */}
         <div className="flex flex-col w-full max-w-[800px]">
-          <div className="flex flex-col items-start md:flex-row justify-between py-2">
+          <div className="flex flex-col items-start md:flex-row justify-between py-2 ">
             <h1 className="font-poppins text-white font-normal text-xl mb-4">
               Produtos na categoria{" "}
               <span className="text-[#F2DD52]">&quot;Nome da Categoria&quot;</span>
@@ -145,20 +143,5 @@ const CategoriesWithoutProducts = ({ categories }: Props) => {
     </>
   );
 };
-
-// Função para gerar dados de categorias estaticamente
-export async function getStaticProps() {
-  const categories: Category[] = [
-    { name: "Categoria 1" },
-    { name: "Categoria 2" },
-    { name: "Categoria 3" },
-  ];
-
-  return {
-    props: {
-      categories,
-    },
-  };
-}
 
 export default CategoriesWithoutProducts;
